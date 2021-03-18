9515833405c1ae6f198ed92313264bfbe0693f38
tugrul <tugrul.alpdogan@dominospizza.com.tr>
2021-03-18 12:16:34 -0600

    prerender io

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const prerender = require('prerender-node'); 
-var morgan = require('morgan')
+const prerenderServer = require('prerender');
+var morgan = require('morgan');
 
 
 const prerenderToken = process.env.PRERENDER_TOKEN;
@@ -10,13 +11,15 @@ console.log("prerender", prerenderServiceUrl, prerenderToken);
 
 const app = express();
 
+
 app.use(morgan(":method :url :status :response-time ms - :res[content-length]"));
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, './build')));
+
 app.use(prerender
     .set('prerenderToken', prerenderToken)
     .set('prerenderServiceUrl', prerenderServiceUrl)
-    );
+);
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/build/index.html'));
@@ -25,3 +28,7 @@ app.get('*', (req, res) => {
 const port = process.env.PORT || 5000;
 app.listen(port);
 console.log(`Server listening on ${port}`);
+
+
+const renderServer = prerenderServer();
+renderServer.start();
