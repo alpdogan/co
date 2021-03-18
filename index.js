188df644a81903ff774cf9d54c83b788de3d82ed
tugrul <tugrul.alpdogan@dominospizza.com.tr>
2021-03-18 09:43:56 -0600

    prerender

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path');
+const prerender = require('prerender-node'); 
+var morgan = require('morgan')
+
+
+const prerenderToken = process.env.PRERENDER_TOKEN;
+
+console.log("prerenderToken", prerenderToken);
 
 const app = express();
 
+app.use(morgan(":method :url :status :response-time ms - :res[content-length]"));
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, './build')));
+app.use(prerender.set('prerenderToken', prerenderToken));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/build/index.html'));
